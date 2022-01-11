b85f5b7a703ab428ef1e8ae6dc7652c73c0dc665
VictorAdelmo <victor_adelmo03@outlook.com>
2022-01-11 17:36:48 -0300

    up

--- routes/rota.js
+++ routes/rota.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controller/controller")
 
-router.get("/characters", controller.getAllPers);
+router.get("/", controller.getAllPers);
 router.get("/characters/:id", controller.getPerId);
 router.post("/characters", controller.cadastrar);
 router.put("/characters/:id", controller.updatePerId);
